1fc59653e7aae53cf348b09ff0739196278b89dd
igoraserpac <igoraserpac@gmail.com>
2023-10-22 16:40:53 -0300

    implementações: Terreno, Robo, Celula

--- src/Terreno/Terreno.java
+++ src/Terreno/Terreno.java
@@ -1,14 +1,34 @@
 package Terreno;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Terreno {
+
+    public Map<Character, Character> direita;
+    public Map<Character, Character> esquerda;
     public Celula[][] terreno;
     public int x;
     public int y;
     public Terreno(int x1, int y1){
         this.x = x1;
         this.y = y1;
+        direita = new HashMap<>();
+        direita.put('N', 'L');
+        direita.put('L', 'S');
+        direita.put('S', 'O');
+        direita.put('O', 'N');
+
+        esquerda = new HashMap<>();
+        esquerda.put('N', 'O');
+        esquerda.put('L', 'N');
+        esquerda.put('S', 'L');
+        esquerda.put('O', 'S');
+
     }
     public void CriarTerreno(){
+
+
         this.terreno = new Celula[this.x][this.y];
         for (int i = 0; i < this.x; i++){
             for (int j = 0; j < this.y; j++){
