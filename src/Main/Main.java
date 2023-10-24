3e08d10a4cca50a94ed19da3b25a0245bdbaab0b
Belle <79115662+Isabelle-Matos@users.noreply.github.com>
2023-10-23 22:24:56 -0300

    controlador (#12)
    * src
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    ---------
    
    Co-authored-by: Igor Cunha <igoraserpac@gmail.com>

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,22 +15,79 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void imprimir(Robo gpt){
-        for(int i = 4; i >= 0; i--){
-            for(int j = 0; j < 5; j++){
-                if(gpt.posicao().get("linha") == i && gpt.posicao().get("coluna") == j){
-                    if(gpt.direcao == 'N') System.out.print("^");
-                    else if(gpt.direcao == 'S') System.out.print("v");
-                    else if(gpt.direcao == 'L') System.out.print(">");
-                    else if(gpt.direcao == 'O') System.out.print("<");
-                }else System.out.print('*');
-            }
-            System.out.print('\n');
-        }
-        System.out.println(gpt.barris);
-        System.out.print('\n');
-    }
+//    public static void imprimir(Robo gpt){
+//        for(int i = 4; i >= 0; i--){
+//            for(int j = 0; j < 5; j++){
+//                if(gpt.posicao().get("linha") == i && gpt.posicao().get("coluna") == j){
+//                    if(gpt.direcao == 'N') System.out.print("^");
+//                    else if(gpt.direcao == 'S') System.out.print("v");
+//                    else if(gpt.direcao == 'L') System.out.print(">");
+//                    else if(gpt.direcao == 'O') System.out.print("<");
+//                }else System.out.print('*');
+//            }
+//            System.out.print('\n');
+//        }
+//        System.out.println(gpt.barris);
+//        System.out.print('\n');
+//    }
     public static void main(String[] args){
+        Terreno T = new Terreno(5, 5);
+        T.CriarTerreno();
+//        for (int linha = 4; linha >= 0; linha--){
+//            for (int coluna = 0; coluna < 5; coluna++){
+//                System.out.println(T.terreno[linha][coluna].pos.get("linha") + " "+ T.terreno[linha][coluna].pos.get("coluna"));
+//                System.out.println(T.terreno[linha][coluna].Concentracao() + " " + T.terreno[linha][coluna].Rugosidade());
+//            }
+//        }
+//        Robo gpt = new Robo();
+//        gpt.atual = T.terreno[1][1];
+        Controlador c = new Controlador(3, T);
+        c.play();
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.GirarDireita();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.GirarEsquerda();
+//        imprimir(gpt);
+//        gpt.GirarEsquerda();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.GirarEsquerda();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+//        gpt.Andar(T);
+//        imprimir(gpt);
+//        gpt.Sondar();
+//        imprimir(gpt);
+
+
 
         int tamanho_terreno = 0;//variavel inteira para campo
         int numero_equipes = 0;//variavel inteira para nEquipes
@@ -69,6 +126,7 @@ public class Main {
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+
 
     }
 
